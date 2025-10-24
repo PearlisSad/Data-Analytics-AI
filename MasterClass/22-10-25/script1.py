846af5ffad85be9f0f84ee6e843df88de5c39c38
Raphael Magrina <raphaelmiguelb@gmail.com>
2025-10-24 09:40:52 +0100

    added files from laptop when I couldnt work on main PC

---
+++ MasterClass/22-10-25/script1.py
@@ -0,0 +1,19 @@
+def add_two_numbers(numb1, numb2):
+    """
+    This functions adds two numbers
+
+    Parameters
+
+    -----
+    numb1: numeric
+    The first number to add
+
+    numb2: numeric
+    The second number to add
+
+    Returns
+    -----
+    
+    """
+    
+    
